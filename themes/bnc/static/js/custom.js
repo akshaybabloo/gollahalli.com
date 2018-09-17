60a80a0d134b6fbbcfa19c6045dd17526bd80151
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2018-09-18 11:55:29 +1200

    checks for id's added

--- themes/bnc/static/js/custom.js
+++ themes/bnc/static/js/custom.js
@@ -8,53 +8,65 @@ const search = instantsearch({
     },
     searchFunction: function (helper) {
         if (helper.state.query === '') {
-            document.querySelector('#hits').innerHTML = 'Type to search...';
-            document.querySelector('#hits-sidenav').innerHTML = 'Type to search...';
+            if (document.getElementById("hits") != null) {
+                document.querySelector('#hits').innerHTML = 'Type to search...';
+            }
+            if (document.getElementById("hits-sidenav") != null) {
+                document.querySelector('#hits-sidenav').innerHTML = 'Type to search...';
+            }
             return;
         }
         helper.search();
     }
 });
 
-search.addWidget(
-    instantsearch.widgets.searchBox({
-        container: '#search-input',
-        placeholder: 'Search...',
-        magnifier: false,
-        reset: false
-    })
-);
+if (document.getElementById("search-input") != null) {
+    search.addWidget(
+        instantsearch.widgets.searchBox({
+            container: '#search-input',
+            placeholder: 'Search...',
+            magnifier: false,
+            reset: false
+        })
+    );
+}
 
-search.addWidget(
-    instantsearch.widgets.hits({
-        container: '#hits',
-        templates: {
-            empty: 'No results',
-            item: '> <a class="gol-links" href="{{{uri}}}">{{{title}}}</a>'
-        },
-        escapeHits: true
-    })
-);
+if (document.getElementById("hits") != null) {
+    search.addWidget(
+        instantsearch.widgets.hits({
+            container: '#hits',
+            templates: {
+                empty: 'No results',
+                item: '> <a class="gol-links" href="{{{uri}}}">{{{title}}}</a>'
+            },
+            escapeHits: true
+        })
+    );
+}
 
-search.addWidget(
-    instantsearch.widgets.searchBox({
-        container: '#search-input-sidenav',
-        placeholder: 'Search...',
-        magnifier: false,
-        reset: false
-    })
-);
+if (document.getElementById("search-input-sidenav") != null) {
+    search.addWidget(
+        instantsearch.widgets.searchBox({
+            container: '#search-input-sidenav',
+            placeholder: 'Search...',
+            magnifier: false,
+            reset: false
+        })
+    );
+}
 
-search.addWidget(
-    instantsearch.widgets.hits({
-        container: '#hits-sidenav',
-        templates: {
-            empty: 'No results',
-            item: '> <a class="gol-links" href="{{{uri}}}">{{{title}}}</a>'
-        },
-        escapeHits: true
-    })
-);
+if (document.getElementById("hits-sidenav") != null) {
+    search.addWidget(
+        instantsearch.widgets.hits({
+            container: '#hits-sidenav',
+            templates: {
+                empty: 'No results',
+                item: '> <a class="gol-links" href="{{{uri}}}">{{{title}}}</a>'
+            },
+            escapeHits: true
+        })
+    );
+}
 
 
 search.start();
