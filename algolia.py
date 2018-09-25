1ceed3ea918beed01d6258300490a932c913a365
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2018-09-25 17:26:20 +1200

    code clean

--- algolia.py
+++ algolia.py
@@ -2,8 +2,13 @@ from algoliasearch import algoliasearch
 import os
 import json
 
-client = algoliasearch.Client("UT1XVMZE1Q", os.environ.get('ALGOLIA_KEY'))
-index = client.init_index('gollahalli-website')
+app_id = "UT1XVMZE1Q"
+index_name = "gollahalli-website"
+file_path = os.path.join('public', 'searchindex.json')
 
-batch = json.load(open(os.path.join('public', 'searchindex.json')))
+client = algoliasearch.Client(app_id, os.environ.get('ALGOLIA_KEY'))
+index = client.init_index(index_name)
+
+batch = json.load(open(file_path))
 index.add_objects(batch)
+print("Successfully uploaded index to Algolia.")
