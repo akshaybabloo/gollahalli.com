82f2157ef4f5395bc9b996681acfd3a3b88674fb
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2018-09-25 17:26:35 +1200

    powershell standards followed

--- script.ps1
+++ script.ps1
@@ -1,46 +1,45 @@
 param(
-    [string]$build,
-    [bool]$deploy,
+    [string]$Build,
+    [bool]$Deploy
 )
 
 Import-Module .\secrets.ps1
 
-function log {
+function Log {
     param (
-        [string]$text
+        [string]$Text
     )
 
-    Write-Host $text -ForegroundColor Magenta
+    Write-Host $Text -ForegroundColor Magenta
 }
 function BuildHugo {
-    log("Building Hugo for Producation...")
+    Log -Text"Building Hugo for Producation..."
     $Env:HUGO_ENV = "production"
 
     $public_folder = Join-Path -Path $PSScriptRoot -ChildPath "public"
 
     if (Test-Path -Path "$public_folder" -ErrorAction SilentlyContinue) {
-        log("'public' folder found, deleting it...")
+        Log -Text "'public' folder found, deleting it..."
         Remove-Item -Path "$public_folder" -Recurse
-        log("'public' folder deleted...")
+        Log -Text "'public' folder deleted..."
         Start-Sleep -Seconds 1
     }
 
-    log("Building website...")
+    Log -Text "Building website..."
     hugo -v --minify --gc
-    log("Website built @ $public_folder")
+    Log -Text "Website built @ $public_folder"
 }
 
-if ($build -eq "production") {
+if ($Build -eq "production") {
     BuildHugo
 }
 
-if ($deploy) {
+if ($Deploy) {
     BuildHugo
 
-    log("Deploying website...")
+    Log -Text "Deploying website..."
     firebase deploy
 
-    log("Uploading search index to Algolia...")
+    Log -Text "Uploading search index to Algolia..."
     python .\algolia.py
-    log("Done!")
 }
