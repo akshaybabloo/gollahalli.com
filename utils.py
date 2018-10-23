7d57c314d2d6217a7bd9472f59ed4b2423e7df1b
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2018-10-23 17:06:22 +1300

    google ping added

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
-from algoliasearch import algoliasearch
-import os
 import json
+import os
+from urllib.parse import urlencode
+from urllib.request import urlopen
+
+try:
+    from algoliasearch import algoliasearch
+except ImportError:
+    print("Algolia Search package does not exist - pip install algoliasearch")
+
+try:
+    import toml
+except ImportError:
+    print("TOML package does not exist - pip install toml")
+
+# ------- Algolia Start ---------
 
 app_id = "UT1XVMZE1Q"
 index_name = "gollahalli-website"
@@ -12,3 +25,16 @@ index = client.init_index(index_name)
 batch = json.load(open(file_path))
 index.add_objects(batch)
 print("Successfully uploaded index to Algolia.")
+
+# ------- Algolia End ---------
+
+# ------- Google Ping Start ---------
+
+PING_URL = "https://www.google.com/webmasters/tools/ping"
+
+config = toml.load("./config.toml")
+
+params = urlencode({'sitemap': config["baseURL"] + "sitemap.xml"})
+urlopen('%s?%s' % (PING_URL, params))
+
+# ------- Google Ping End ---------
