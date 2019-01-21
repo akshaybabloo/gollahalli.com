334b9732bea61329a56041a1f1d7d9361b32a2b4
akshay raj gollahalli <akshaybabloo@gmail.com>
2019-01-21 18:44:09 +0530

    clear entries before adding new ones

--- utils.py
+++ utils.py
@@ -22,6 +22,8 @@ file_path = os.path.join('public', 'searchindex.json')
 client = algoliasearch.Client(app_id, os.environ.get('ALGOLIA_KEY'))
 index = client.init_index(index_name)
 
+index.clear_index()  # Clear previous entries.
+
 batch = json.load(open(file_path))
 index.add_objects(batch)
 print("Successfully uploaded index to Algolia.")
