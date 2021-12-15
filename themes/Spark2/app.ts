05802e99e4122058233352cb0f24be9743e37068
Akshay Raj Gollahalli <akshay@gollahalli.com>
2021-12-16 11:06:20 +1300

    search icon added

--- themes/Spark2/app.ts
+++ themes/Spark2/app.ts
@@ -1,14 +1,16 @@
 import './main.css';
 import {icon, library, } from '@fortawesome/fontawesome-svg-core'
 import {faGithub, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import Vue from "vue";
 
 
-library.add(faGithub, faTwitter, faLinkedin);
+library.add(faGithub, faTwitter, faLinkedin, faSearch);
 
 const linkedin = icon({prefix: 'fab', iconName: 'linkedin'}, {transform: {size: 30}});
 const github = icon({prefix: 'fab', iconName: 'github'}, {transform: {size: 30}});
 const twitter = icon({prefix: 'fab', iconName: 'twitter'}, {transform: {size: 30}});
+const search = icon({prefix: 'fas', iconName: 'search'}, {transform: {size: 20}});
 
 new Vue({
     el: '#profile',
@@ -16,6 +18,7 @@ new Vue({
         linkedin: linkedin.html.pop(),
         github: github.html.pop(),
         twitter: twitter.html.pop(),
+        search: search.html.pop(),
         showMenu: true,
     },
     methods: {
