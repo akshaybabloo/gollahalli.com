ec471867fa87707414fb4894458a745f1419ca8e
Akshay Raj Gollahalli <akshay@gollahalli.com>
2021-12-17 17:24:24 +1300

    search init

--- themes/Spark2/app.ts
+++ themes/Spark2/app.ts
@@ -1,16 +1,17 @@
 import './main.css';
 import {icon, library, } from '@fortawesome/fontawesome-svg-core'
 import {faGithub, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faSearch, faTimes} from '@fortawesome/free-solid-svg-icons';
 import Vue from "vue";
 
 
-library.add(faGithub, faTwitter, faLinkedin, faSearch);
+library.add(faGithub, faTwitter, faLinkedin, faSearch, faTimes);
 
 const linkedin = icon({prefix: 'fab', iconName: 'linkedin'}, {transform: {size: 30}});
 const github = icon({prefix: 'fab', iconName: 'github'}, {transform: {size: 30}});
 const twitter = icon({prefix: 'fab', iconName: 'twitter'}, {transform: {size: 30}});
 const search = icon({prefix: 'fas', iconName: 'search'}, {transform: {size: 20}});
+const times = icon({prefix: 'fas', iconName: 'times'}, {transform: {size: 20}});
 
 new Vue({
     el: '#profile',
@@ -19,12 +20,17 @@ new Vue({
         github: github.html.pop(),
         twitter: twitter.html.pop(),
         search: search.html.pop(),
+        times: times.html.pop(),
+
         showMenu: true,
+        showSearch: true
     },
     methods: {
         showMenuToggle: function () {
             this.showMenu = !this.showMenu;
-            console.log(this.showMenu);
+        },
+        showSearchToggle: function () {
+            this.showSearch = !this.showSearch;
         }
     }
 });
