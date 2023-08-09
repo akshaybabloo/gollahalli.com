09a177df742e859f6f1e4235b3dfce105180c447
Akshay Raj Gollahalli <akshay.gollahalli@fisherpaykel.com>
2023-08-09 14:40:18 +1200

    add gif support to powershell script

--- scripts/Convert-WebP.ps1
+++ scripts/Convert-WebP.ps1
@@ -1,43 +1,51 @@
 $scriptLocation = Split-Path $MyInvocation.MyCommand.Path
 $parentDirectory = Split-Path $scriptLocation -Parent
 
-$imageExtensions = @('*.jpg', '*.png')
+$imageExtensions = @('*.jpg', '*.png', '*.gif')
 
 $results = [System.Collections.Generic.List[PSCustomObject]]::new()
 
-Write-Output "Converting images to .webp format in $parentDirectory"
-
 foreach ($extension in $imageExtensions) {
+    Write-Progress -Activity "Converting images to .webp format in $parentDirectory" -PercentComplete 0
+
     $imageFiles = Get-ChildItem -Path $parentDirectory -Filter $extension -File -Recurse
 
     $imageFiles | ForEach-Object -Parallel {
         $newFileName = [IO.Path]::ChangeExtension($_.Name, ".webp")
 
-        $originalSize = if ($_.Length -ge 1MB) {"{0:N2} MB" -f ($_.Length / 1MB)} else {"{0:N2} KB" -f ($_.Length / 1KB)}
+        $originalSize = if ($_.Length -ge 1MB) { "{0:N2} MB" -f ($_.Length / 1MB) } else { "{0:N2} KB" -f ($_.Length / 1KB) }
 
-        $null = & 'cwebp' -q 75 $_.FullName -o (Join-Path $_.Directory.FullName $newFileName) 2>&1
+        $conversionError = $null
+        if ($extension -eq '*.gif') {
+            $conversionError = & 'gif2webp' -mt -lossy -q 75 $_.FullName -o (Join-Path $_.Directory.FullName $newFileName) 2>&1
+        }
+        else {
+            $conversionError = & 'cwebp' -mt -q 75 $_.FullName -o (Join-Path $_.Directory.FullName $newFileName) 2>&1
+        }
 
         if ($LASTEXITCODE -eq 0) {
             $newSize = (Get-Item (Join-Path $_.Directory.FullName $newFileName)).Length
-            $newSize = if ($newSize -ge 1MB) {"{0:N2} MB" -f ($newSize / 1MB)} else {"{0:N2} KB" -f ($newSize / 1KB)}
+            $newSize = if ($newSize -ge 1MB) { "{0:N2} MB" -f ($newSize / 1MB) } else { "{0:N2} KB" -f ($newSize / 1KB) }
 
             $originalFileName = if ($_.Name.Length -gt 30) { $_.Name.Substring(0, 27) + "..." } else { $_.Name }
             $newFileNameTruncated = if ($newFileName.Length -gt 30) { $newFileName.Substring(0, 27) + "..." } else { $newFileName }
 
             [PSCustomObject]@{
                 'OriginalFile' = $originalFileName
-                'NewFile' = $newFileNameTruncated
+                'NewFile'      = $newFileNameTruncated
                 'OriginalSize' = $originalSize
-                'NewSize' = $newSize
-                'Successful' = [char]0x2705
+                'NewSize'      = $newSize
+                'Successful'   = [char]0x2705
             }
-        } else {
+        }
+        else {
+            Write-Output "Failed to convert $($_.Name) ($originalSize) to $newFileName. Error: $conversionError"
             [PSCustomObject]@{
                 'OriginalFile' = $originalFileName
-                'NewFile' = $newFileNameTruncated
+                'NewFile'      = $newFileNameTruncated
                 'OriginalSize' = $originalSize
-                'NewSize' = $null
-                'Successful' = [char]0x274C
+                'NewSize'      = $null
+                'Successful'   = [char]0x274C
             }
         }
     } -ThrottleLimit 5 | ForEach-Object { $results.Add($_) }
